2a3675f6741ac758713202a9e616cabd49cbc321
pcouy <couy.pierre@gmail.com>
2021-11-22 15:47:22 +0100

    Pin gym version

--- code/setup.py
+++ code/setup.py
@@ -5,5 +5,5 @@ setup(
     name="M2-IABD-DeepRL",
     version="1.0",
     packages=find_packages(),
-    install_requires=['gym','numpy','matplotlib','pillow','pyqt5']
+    install_requires=['gym==0.21.0','numpy','matplotlib','pillow','pyqt5']
 )
